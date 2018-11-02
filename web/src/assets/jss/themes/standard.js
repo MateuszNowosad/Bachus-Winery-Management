f165e41df13c887ea3a6454e5c77ab3be89d51e6
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-11-02 18:06:19 +0000

    01 web

---
+++ web/src/assets/jss/themes/standard.js
@@ -0,0 +1,18 @@
+export const standard = {
+    palette: {
+        primary: {
+            main: '#9ccc65',
+        },
+        secondary: {
+            main: '#ab47bc',
+        },
+        common: {
+            grey: '#9E9E9E',
+        },
+        success: {
+            main: '#32CD32',
+        }
+    },
+};
+
+export const drawerWidth = 240;
---
+++ web/src/components/AutoTable/AutoContent.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TableCell from "@material-ui/core/TableCell/TableCell";
+import TableRow from "@material-ui/core/TableRow/TableRow";
+import TableBody from "@material-ui/core/TableBody/TableBody";
+
+
+const AutoContent = (props) => {
+    let row = [];
+    props.data["data"][props.subject].map(currElement => {
+        let values = Object.values(currElement);
+        let cells = [];
+        for (let value in values) {
+            let uniqueCellID = values[0] + 'cell' + value;
+            cells.push(<TableCell key={uniqueCellID}> {values[value]} </TableCell>);
+        }
+        row.push(<TableRow key={values[0]}>{cells}</TableRow>);
+    });
+    return (
+        <TableBody>
+            {row}
+        </TableBody>
+    );
+};
+
+AutoContent.propTypes = {
+    data: PropTypes.object.isRequired,
+    subject: PropTypes.string.isRequired,
+};
+
+export default AutoContent;
