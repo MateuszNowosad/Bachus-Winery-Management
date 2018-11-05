cfe99fd8519c413e8581976bdd4a084d626e118c
Jakub <mielniczuk.jakub@gmail.com>
2018-11-05 20:48:43 +0100

    Added FormOperations

---
+++ web/src/views/common/forms/FormOperations.js
@@ -0,0 +1,244 @@
+import React from 'react';
+import { Grid, MenuItem, TextField } from '@material-ui/core';
+import PropTypes from 'prop-types';
+
+const operations = ['fermentacja', 'dojrzewanie'];
+
+export class FormOperations extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beginAmount: null,
+      endAmount: null,
+      beginDate: '',
+      endDate: '',
+      alcoholContent: '',
+      additiveAmount: null,
+      sugarContent: null,
+      acidity: null,
+      temperature: null,
+      desc: '',
+      process: ''
+    };
+  }
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
+
+  handleSubmit = () => {
+    const {
+      beginAmount,
+      endAmount,
+      beginDate,
+      endDate,
+      alcoholContent,
+      additiveAmount,
+      sugarContent,
+      acidity,
+      temperature,
+      desc,
+      process
+    } = this.state;
+    this.props.onSubmit({
+      beginAmount,
+      endAmount,
+      beginDate,
+      endDate,
+      alcoholContent,
+      additiveAmount,
+      sugarContent,
+      acidity,
+      temperature,
+      desc,
+      process
+    });
+    this.props.formSubmitted();
+  };
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.submitFromOutside && this.props.submitFromOutside) {
+      this.handleSubmit();
+    }
+  }
+
+  render() {
+    const {
+      beginAmount,
+      endAmount,
+      beginDate,
+      endDate,
+      alcoholContent,
+      additiveAmount,
+      sugarContent,
+      acidity,
+      temperature,
+      desc,
+      process
+    } = this.state;
+
+    return (
+      <form style={{ margin: '0% 25%' }}>
+        <Grid container spacing={8} justify={'center'}>
+          <Grid item md={12}>
+            <TextField
+              fullWidth
+              id="process"
+              select
+              label="Rodzaj operacji"
+              placeholder="Rodzaj operacji"
+              value={process}
+              onChange={this.handleChange('process')}
+              margin="dense"
+              variant={'outlined'}
+            >
+              {operations.map(option => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item md={6}>
+            <TextField
+              fullWidth
+              id="beginAmount"
+              label="Ilość początkowa"
+              value={beginAmount}
+              margin="dense"
+              type="number"
+              onChange={this.handleChange('beginAmount')}
+              variant={'outlined'}
+            />
+          </Grid>
+          <Grid item md={6}>
+            <TextField
+              fullWidth
+              id="endAmount"
+              label="Ilość końcowa"
+              value={endAmount}
+              margin="dense"
+              type="number"
+              onChange={this.handleChange('endAmount')}
+              variant={'outlined'}
+            />
+          </Grid>
+          <Grid item md={6}>
+            <TextField
+              fullWidth
+              id="beginDate"
+              label="Data początku"
+              type="date"
+              value={beginDate}
+              InputLabelProps={{
+                shrink: true
+              }}
+              margin="dense"
+              onChange={this.handleChange('beginDate')}
+              variant={'outlined'}
+            />
+          </Grid>
+          <Grid item md={6}>
+            <TextField
+              fullWidth
+              id="endDate"
+              label="Data zakończenia"
+              type="date"
+              value={endDate}
+              InputLabelProps={{
+                shrink: true
+              }}
+              margin="dense"
+              onChange={this.handleChange('endDate')}
+              variant={'outlined'}
+            />
+          </Grid>
+          <Grid item md={6}>
+            <TextField
+              fullWidth
+              id="alcoholContent"
+              label="Zawartość alkoholu"
+              value={alcoholContent}
+              margin="dense"
+              type="number"
+              onChange={this.handleChange('alcoholContent')}
+              variant={'outlined'}
+            />
+          </Grid>
+          <Grid item md={6}>
+            <TextField
+              fullWidth
+              id="additiveAmount"
+              label="Ilość dodatku"
+              value={additiveAmount}
+              margin="dense"
+              type="number"
+              onChange={this.handleChange('additiveAmount')}
+              variant={'outlined'}
+            />
+          </Grid>
+          <Grid item md={6}>
+            <TextField
+              fullWidth
+              id="sugarContent"
+              label="Zawartość cukru"
+              value={sugarContent}
+              margin="dense"
+              type="number"
+              onChange={this.handleChange('sugarContent')}
+              variant={'outlined'}
+            />
+          </Grid>
+          <Grid item md={6}>
+            <TextField
+              fullWidth
+              id="acidity"
+              label="Kwasowość"
+              value={acidity}
+              margin="dense"
+              type="number"
+              onChange={this.handleChange('acidity')}
+              variant={'outlined'}
+            />
+          </Grid>
+          <Grid item md={6}>
+            <TextField
+              fullWidth
+              id="temperature"
+              label="Temperatura"
+              value={temperature}
+              margin="dense"
+              type="number"
+              onChange={this.handleChange('temperature')}
+              variant={'outlined'}
+            />
+          </Grid>
+          <Grid item md={12}>
+            <TextField
+              fullWidth
+              id="desc"
+              label="Opis"
+              placeholder="Opis"
+              value={desc}
+              multiline
+              margin="dense"
+              onChange={this.handleChange('desc')}
+              variant={'outlined'}
+              inputProps={{
+                maxLength: '255'
+              }}
+            />
+          </Grid>
+        </Grid>
+      </form>
+    );
+  }
+}
+
+FormOperations.propTypes = {
+  submitFromOutside: PropTypes.bool,
+  onSubmit: PropTypes.func,
+  formSubmitted: PropTypes.func
+};
