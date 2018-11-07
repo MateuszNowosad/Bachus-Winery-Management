05f90cf42db247e97b4b52bac59c3e95e245a0c1
Jakub Mielniczuk <mielniczuk.jakub@gmail.com>
2018-11-07 18:11:49 +0100

    Modified currentDate to return two types of data, with or without time.

--- web/src/views/common/forms/CurrentDate.js
+++ web/src/views/common/forms/CurrentDate.js
@@ -1,6 +1,17 @@
-function currentDate() {
+import PropTypes from 'prop-types';
+
+function currentDate(variant) {
   const date = new Date();
-  return date.toISOString().slice(0, 16);
+  switch (variant) {
+    case 'date':
+      return date.toISOString().slice(0, 10);
+    case 'dateTime':
+      return date.toISOString().slice(0, 16);
+  }
 }
+
+currentDate.propTypes = {
+  variant: PropTypes.string.isRequired
+};
 
 export default currentDate;
