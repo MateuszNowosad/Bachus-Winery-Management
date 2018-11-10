973f96600a5e96e1e7d85103eefff251f4c06dff
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-11-10 13:56:50 +0100

    Added example user.

---
+++ web/src/variables/AdminDashboard/ExampleUser.js
@@ -0,0 +1,25 @@
+const userData = {
+  userData: {
+    user: [
+      {
+        idUzytkownika: '0',
+        imie: 'Jan',
+        nazwisko: 'Kowalski',
+        login: 'JKow',
+        haslo: 's',
+        PESEL: '78012957935',
+        eMail: 'mateusz.nowosad1@gmail.com',
+        nrTelefonu: '+49123456789',
+        dataOstatniegoLogowania: '13:54 10.11.2018',
+        zdjecie: '/asset/img/example.jpg',
+        czyAktywne: 'tak',
+        adres: 'Szymanowskiego 15, 22-100 Chełm',
+        rola: 'Admin',
+        createdAt: '13:00 10.11.2018',
+        updatedAt: '13:10 10.11.2018'
+      }
+    ]
+  }
+};
+
+export default userData;
