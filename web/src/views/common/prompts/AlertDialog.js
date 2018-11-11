60d1f32cb1ca89ad4df1d0c90461d376193b3c0b
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-11-11 19:18:31 +0100

    New component AlertDialog.

---
+++ web/src/views/common/prompts/AlertDialog.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+class AlertDialog extends React.Component {
+    state = {
+        open: this.props.open,
+    };
+
+    handleCancel = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        const {dialogTitle, handleAgree, dialogMessage} = this.props;
+        return (
+            <div>
+                <Dialog
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{dialogTitle}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            {dialogMessage}
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleCancel} color="primary">
+                            Anuluj
+                        </Button>
+                        <Button onClick={handleAgree} color="primary" autoFocus>
+                            Wykonaj
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+AlertDialog.propTypes = {
+    dialogTitle: PropTypes.string.isRequired,
+    dialogMessage: PropTypes.string.isRequired,
+    handleAgree: PropTypes.func.isRequired,
+    open: PropTypes.bool.isRequired,
+};
+
+export default AlertDialog;
