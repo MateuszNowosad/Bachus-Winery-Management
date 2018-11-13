409211af5b9ab1c5b7f37ee147bb0744fa7c9375
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-11-13 21:42:39 +0100

    New validation keys for FormVineyardOperation.

--- web/src/views/common/forms/UniversalValidationHandler/validationKeys/validationKeys.js
+++ web/src/views/common/forms/UniversalValidationHandler/validationKeys/validationKeys.js
@@ -89,3 +89,8 @@ export const warehouseValidationKeys = {
     type: /^[1-3]$/,
     capacity: /^(\d|\d+\.\d+){1,7}$/
 };
+
+export const vineyardOperationsValidationKeys = {
+    dateOfOperation: /^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2})$/,
+    desc: new RegExp("^(|[\\s\\S]{2,45})$","u"),
+};
