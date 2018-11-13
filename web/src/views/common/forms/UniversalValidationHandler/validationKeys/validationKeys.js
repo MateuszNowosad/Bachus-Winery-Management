8b8ceb270ce60f003c29f1dc7394294b5eda2b64
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-11-13 20:00:57 +0100

    Cleaned up validation keys.

--- web/src/views/common/forms/UniversalValidationHandler/validationKeys/validationKeys.js
+++ web/src/views/common/forms/UniversalValidationHandler/validationKeys/validationKeys.js
@@ -1,24 +1,22 @@
-const workaroundRegex = new RegExp("^\\p{L}{2,30}$","u");
-
 export const usersValidationKeys = {
-  firstName: workaroundRegex,
-    lastName: workaroundRegex,
-    login: /\w{4,10}/,
-  password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
-  PESEL: /\d{11}/,
-  eMail:
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g,
-  phoneNumber: /\d{11}/g,
-  photo: /.+/g,
-  userRole: /.+/g,
-  imagePreviewUrl: /.+/g
+    firstName: new RegExp("^\\p{L}{2,30}$","u"),
+    lastName: new RegExp("^\\p{L}{2,30}$","u"),
+    login: /^\w{4,10}$/,
+    password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})$/,
+    PESEL: /^\d{11}$/,
+    eMail:
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g,
+    phoneNumber: /^\d{11}$/,
+    photo: /^.+$/,
+    userRole: /^.+$/,
+    imagePreviewUrl: /^.+$/
 };
 
 export const addressValidationKeys = {
     street: new RegExp("^\\p{L}{2,45}$","u"),
-    buildingNumber: /\d{1,4}/,
-    apartmentNumber: /^(\s*|\d{1,2})$/,
-    postalCode: /^(\s*|\d{1,10})$/,
+    buildingNumber: /^\d{1,4}$/,
+    apartmentNumber: /^(|\d{1,2})$/,
+    postalCode: /^(|\d{1,10})$/,
     city: new RegExp("^\\p{L}{2,20}$","u"),
     country: new RegExp("^\\p{L}{2,60}$","u")
 };
@@ -26,5 +24,5 @@ export const addressValidationKeys = {
 export const categoriesDictValidationKeys = {
     name: new RegExp("^\\p{L}{3,20}$","u"),
     unit: new RegExp("^\\p{L}{2,20}$","u"),
-    desc: new RegExp("(^$|[\\s\\S]{2,250})","u")
+    desc: new RegExp("^(|[\\s\\S]{2,250})$","u")
 };
