79dafd8ac0f1df437f8df7f6b24dfc214ad2f452
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-11-13 14:18:55 +0100

    New UniversalValidationHandler function.

---
+++ web/src/views/common/forms/UniversalValidationHandler/UniversalValidationHandler.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+function UniversalValidationHander(dataObject, validationKeys) {
+  let dataErrorFields = [];
+  let entries = Object.entries(dataObject);
+  for (let entry in entries) {
+    let test = validationKeys[entries[entry][0]].test(entries[entry][1]);
+    if (!test){
+      dataErrorFields.push(entries[entry][0]);
+    }
+  }
+  return dataErrorFields;
+}
+
+UniversalValidationHander.propTypes = {
+  dataObject: PropTypes.object.isRequired
+};
+
+export default UniversalValidationHander;
