103fa2c0d9f4fbf6f764a0b6e2a0b429ec992178
Filip Mazur <mazurfilip13@gmail.com>
2018-11-17 22:08:30 +0100

    Test docker script, add instruction to initialize database docker container properly

--- server/scripts/dockerSetup.sh
+++ server/scripts/dockerSetup.sh
@@ -5,7 +5,3 @@ docker pull mysql/mysql-server:5.7
 
 echo "Start mysql container"
 docker run --name=bachus-winery57 -d mysql/mysql-server:5.7
-sleep 5s
-
-echo "Enter container"
-docker exec -it bachus-winery57 mysql -utest -ptest
