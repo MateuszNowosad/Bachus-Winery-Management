81bb98277835955eb3a34eb2976577d21de4c964
Filip Mazur <mazurfilip13@gmail.com>
2018-11-17 21:27:56 +0100

    code cleanup, simplify server construct

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,6 @@
 import express, { urlencoded } from 'express';
 import cors from 'cors';
 import * as bodyParser from 'body-parser';
-import schema from './api/graphql/schema';
-import { repositories } from './api/graphql/repositories';
-
-import passport from 'passport';
-import session from 'express-session';
-const { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
 
 export const createApp = async () => {
   const app = express();
@@ -20,29 +14,8 @@ export const createApp = async () => {
   };
 
   app.use(cors(corsOptions));
-
   app.use(urlencoded({ extended: true }));
-
   app.use(bodyParser.json());
-
-  // not working
-  // app.use(
-  //   '/graphql',
-  //   graphqlExpress(req => ({
-  //     schema,
-  //     context: {
-  //       ...repositories,
-  //       user: req.user,
-  //       req
-  //     }
-  //   }))
-  // );
-  app.use(
-    '/graphiql',
-    graphiqlExpress({
-      endpointURL: '/graphql'
-    })
-  );
 
   return { app };
 };
