39e6733139191c42178c9450da9e4da71cabaf53
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-11-21 18:38:56 +0100

    Removed validation of imagePreviewUrl from FormUser

--- web/src/views/common/forms/UniversalValidationHandler/validationKeys/validationKeys.js
+++ web/src/views/common/forms/UniversalValidationHandler/validationKeys/validationKeys.js
@@ -7,8 +7,7 @@ export const usersValidationKeys = {
   eMail: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   phoneNumber: /^\d{11}$/,
   photo: /^.+$/,
-  userRole: /^.+$/,
-  imagePreviewUrl: /^.+$/
+  userRole: /^.+$/
 };
 
 export const addressValidationKeys = {
