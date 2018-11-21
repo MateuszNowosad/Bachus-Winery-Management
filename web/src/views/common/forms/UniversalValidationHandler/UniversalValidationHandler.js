e3175216c7368dabaab1b22d27f2839540c71723
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-11-21 17:54:12 +0100

    UniversalSubmissioHandler now allows unvalidated fields.

--- web/src/views/common/forms/UniversalValidationHandler/UniversalValidationHandler.js
+++ web/src/views/common/forms/UniversalValidationHandler/UniversalValidationHandler.js
@@ -4,6 +4,7 @@ function UniversalValidationHander(dataObject, validationKeys) {
   let dataErrorFields = [];
   let entries = Object.entries(dataObject);
   for (let entry in entries) {
+      if(validationKeys[entries[entry][0]] === undefined) continue;
     let test = validationKeys[entries[entry][0]].test(entries[entry][1]);
     if (!test){
       dataErrorFields.push(entries[entry][0]);
