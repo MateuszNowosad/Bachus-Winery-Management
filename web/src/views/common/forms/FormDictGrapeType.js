5d4e72dfc836b82c146b63232ba4b9cc6fc3b7dc
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-11-21 19:41:29 +0100

    Added validation for FromDictGrapeType

--- web/src/views/common/forms/FormDictGrapeType.js
+++ web/src/views/common/forms/FormDictGrapeType.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Grid, TextField } from '@material-ui/core';
 import PropTypes from 'prop-types';
+import UniversalValidationHandler from "./UniversalValidationHandler/UniversalValidationHandler";
+import {grapeTypeDictValidationKeys} from "./UniversalValidationHandler/validationKeys/validationKeys";
 
 const errorMap = {
   name: false,
@@ -26,8 +28,22 @@ export class FormDictGrapeType extends React.Component {
 
   handleSubmit = () => {
     const { name, desc } = this.state;
-    this.props.onSubmit({ name, desc });
-    this.props.formSubmitted();
+
+    let dataObject = {
+        name, desc
+      };
+
+      let arrayOfErrors = UniversalValidationHandler(dataObject, grapeTypeDictValidationKeys);
+      if (arrayOfErrors.length === 0) {
+          if (this.props.onSubmit(dataObject)) this.props.formSubmitted();
+      } else{
+          let error = Object.assign({}, errorMap);
+          for (let errorField in arrayOfErrors) {
+              error[arrayOfErrors[errorField]] = true;
+          }
+          this.setState({error: error});
+          this.props.submitAborted();
+      }
   };
 
   componentDidUpdate(prevProps) {
@@ -83,5 +99,6 @@ export class FormDictGrapeType extends React.Component {
 FormDictGrapeType.propTypes = {
   submitFromOutside: PropTypes.bool,
   onSubmit: PropTypes.func,
-  formSubmitted: PropTypes.func
+  formSubmitted: PropTypes.func,
+  submitAborted: PropTypes.func
 };
