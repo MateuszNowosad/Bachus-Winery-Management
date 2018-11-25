cad27952394be4afc35fe87e96fcdb41e14ba34b
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-11-25 18:39:36 +0100

    MediaCard styling changes.

--- web/src/assets/jss/common/components/MediaCardStyle.js
+++ web/src/assets/jss/common/components/MediaCardStyle.js
@@ -1,10 +1,12 @@
-const MediaCardStyle = {
+const MediaCardStyle = theme => ({
     card: {
+        minWidth: 345,
         maxWidth: 345,
+        margin: theme.spacing.unit,
     },
     media: {
         objectFit: 'cover',
     },
-};
+});
 
 export default MediaCardStyle;
