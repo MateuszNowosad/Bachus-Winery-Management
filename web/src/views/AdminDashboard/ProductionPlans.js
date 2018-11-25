aa5670084398af164b3d58cae6c7e89cf08ae956
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-11-25 15:15:54 +0100

    Production plans view skeleton.

---
+++ web/src/views/AdminDashboard/ProductionPlans.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import AdminDashboardStyle from '../../assets/jss/common/views/AdminDashboard/AdminDashboardStyle.js';
+
+class ProductionPlans extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Typography variant="h4" gutterBottom component="h2">
+                    Plany produkcji
+                </Typography>
+            </React.Fragment>
+        );
+    }
+}
+
+ProductionPlans.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(AdminDashboardStyle)(ProductionPlans);
