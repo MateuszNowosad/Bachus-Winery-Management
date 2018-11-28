a24dcc1f38e5c6525715f1e31c58c9233b715a07
Jakub Mielniczuk <mielniczuk.jakub@gmail.com>
2018-11-28 12:04:54 +0100

    PDFFromDataSet fixed nested tables widths and modified to accept data from apollo

--- web/src/components/PDFSchemes/PDFFromDataSet.js
+++ web/src/components/PDFSchemes/PDFFromDataSet.js
@@ -1,95 +1,110 @@
-import React from 'react';
-
+const nestedWidthsType = '*';
 
 const formatLabels = (dataLabels) => {
-  let labels = [];
-  for (let i = 0; i < dataLabels.length; i++) {
-    if (dataLabels[i] instanceof Array) {
-      labels.push({
-        table:
-          {
-            body: [formatLabels(dataLabels[i])]
-          }, margin: [-5, -3, -5, -3]
-      });
-    } else {
-      labels.push({ text: dataLabels[i], bold: true });
+    let labels = [];
+    for (let i = 0; i < dataLabels.length; i++) {
+        if (dataLabels[i] instanceof Array) {
+            let widths = [];
+            let body = formatLabels(dataLabels[i]);
+            for (let j = 0; j < body.length; j++) {
+                widths.push(nestedWidthsType);
+            }
+            labels.push({
+                table:
+                    {
+                        widths: widths,
+                        body: [body]
+                    }, margin: [-5, -3, -5, -3]
+            });
+        } else {
+            labels.push({text: dataLabels[i], bold: true});
+        }
     }
-  }
-  return labels;
+    return labels;
 };
 
 const formatData = (dataObject, dataLabels) => {
-  let data = [];
+    let data = [];
 
-  dataLabels !== undefined ?
-    data.push(formatLabels(dataLabels)) : null;
+    dataLabels !== undefined ?
+        data.push(formatLabels(dataLabels)) : null;
 
 
-  dataObject.length !== 0 ?
-    dataObject.map(currElement => {
-      let values = Object.values(currElement);
-      let cells = [];
-      for (let value in values) {
-        if (values[value] instanceof Object) {
-          if (values[value] instanceof Array) {
-            let arrayValue = Object.values(values[value]);
-            if (arrayValue.length === 0) {
-              cells.push({ text: '' });
-            } else {
-              let body = [];
-              for (let i = 0; i < arrayValue.length; i++) {
-                let bodyCells= formatData([Object.values(arrayValue[i])])[0];
-                let bodyRow = [];
-                for (let j = 0; j < bodyCells.length; j++) {
-                  bodyRow.push(bodyCells[j]);
+    dataObject.length !== 0 ?
+        dataObject.map(currElement => {
+            let values = Object.values(currElement);
+            let cells = [];
+            for (let value in values) {
+                if (value < values.length - 1) {
+                    if (values[value] instanceof Object) {
+                        if (values[value] instanceof Array) {
+                            let arrayValue = Object.values(values[value]);
+                            if (arrayValue.length === 0) {
+                                cells.push({text: ''});
+                            } else {
+                                let body = [];
+                                let widths = [];
+                                for (let i = 0; i < arrayValue.length; i++) {
+                                    let bodyCells = formatData([Object.values(arrayValue[i])])[0];
+                                    let bodyRow = [];
+                                    let tmpWidths = [];
+                                    for (let j = 0; j < bodyCells.length; j++) {
+                                        tmpWidths.push(nestedWidthsType);
+                                        bodyRow.push(bodyCells[j]);
+                                    }
+                                    body.push(bodyRow);
+                                    widths = tmpWidths;
+                                }
+                                cells.push({
+                                    table: {
+                                        widths: widths,
+                                        body: body
+                                    }
+                                    , margin: [-5, -3, -5, -3]
+                                });
+                            }
+                        } else {
+                            let bodyCells = formatData([[values[value]][0]])[0];
+                            let body = [];
+                            let widths = [];
+                            for (let j = 0; j < bodyCells.length; j++) {
+                                body.push(bodyCells[j]);
+                                widths.push(nestedWidthsType);
+                            }
+                            cells.push({
+                                table: {
+                                    widths: widths,
+                                    body: [body]
+                                }
+                                , margin: [-5, -3, -5, -3]
+                            });
+                        }
+                    } else
+                        cells.push({text: values[value]});
                 }
-                body.push(bodyRow);
-              }
-              cells.push({
-                table: {
-                  body: body
-                }
-                , margin: [-5, -3, -5, -3]
-              });
+
             }
-          } else {
-            let bodyCells= formatData([[Object.values(values[value])][0]])[0];
-            let body = [];
-            for (let j = 0; j < bodyCells.length; j++)
-              body.push(bodyCells[j]);
-            cells.push({
-              table: {
-                body: [body]
-              }
-              , margin: [-5, -3, -5, -3]
-            });
-          }
-        } else
-          cells.push({ text: values[value] });
-
-      }
-      data.push(cells);
-    })
-    : data.push(['']);
-
-  return data;
+            data.push(cells);
+        })
+        : data.push(['']);
+    return data;
 };
 
 const PDFFromDataSet = (dataObject, dataLabels) => {
-  return ({
-    pageSize: 'A4',
-    pageOrientation: 'landscape',
-    content: [
-      {
-        table: {
-          dontBreakRows: true,
-          headerRows: 1,
-          widths: 'auto',
-          body: formatData(dataObject, dataLabels)
-        }, fontSize: 5
-      }
-    ]
-  });
+    return ({
+        pageSize: 'A4',
+        pageOrientation: 'landscape',
+        content: [
+            {
+                table: {
+                    dontBreakRows: true,
+                    headerRows: 1,
+                    widths: 'auto',
+                    body: formatData(dataObject, dataLabels)
+                }, fontSize: 5
+            }
+        ]
+    });
 };
 
 export default PDFFromDataSet;
