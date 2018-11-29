3dd45497d716e02f2ac0834d79f1004fbfa5b09d
Jakub Mielniczuk <mielniczuk.jakub@gmail.com>
2018-11-29 22:03:23 +0100

    Added file with style DataToPDFStyle

---
+++ web/src/assets/jss/common/components/DataToPDFStyle.js
@@ -0,0 +1,11 @@
+const DataToPDFStyle = theme => ({
+    list: {
+        maxHeight: 200,
+        overflow: 'auto'
+    },
+    form: {
+        margin: '0% 25%'
+    }
+});
+
+export default DataToPDFStyle;
