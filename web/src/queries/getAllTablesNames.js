9fcca815f226b973f78f688806b296aed661428d
Jakub Mielniczuk <mielniczuk.jakub@gmail.com>
2018-12-01 16:08:31 +0000

    Pdf generator view

---
+++ web/src/queries/getAllTablesNames.js
@@ -0,0 +1,17 @@
+import gql from "graphql-tag"
+
+const getAllTablesNames = gql`
+    {
+        __schema
+        {
+            queryType
+            {
+                tables: fields
+                {
+                    name
+                }
+            }
+        }
+    }`;
+
+export default getAllTablesNames;
