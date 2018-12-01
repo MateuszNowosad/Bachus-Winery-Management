bd241180dc88bd6cb4369d31e18db2e1fcd555a8
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-12-01 16:42:24 +0100

    Improved ExampleRadarChart.

--- web/src/variables/AdminDashboard/ExampleRadarChart.js
+++ web/src/variables/AdminDashboard/ExampleRadarChart.js
@@ -2,23 +2,26 @@ import React from 'react';
 import { Radar, RadarChart, PolarGrid, Legend, PolarAngleAxis, PolarRadiusAxis } from 'recharts';
 
 const data = [
-  { subject: 'Math', A: 120, B: 110, fullMark: 150 },
-  { subject: 'Chinese', A: 98, B: 130, fullMark: 150 },
-  { subject: 'English', A: 86, B: 130, fullMark: 150 },
-  { subject: 'Geography', A: 99, B: 100, fullMark: 150 },
-  { subject: 'Physics', A: 85, B: 90, fullMark: 150 },
-  { subject: 'History', A: 65, B: 85, fullMark: 150 }
+  { subject: 'Butelki', A: 36 },
+  { subject: 'Korki', A: 72 },
+  { subject: 'Wino', A: 99 },
+  { subject: 'Etykiety', A: 32 }
 ];
+
+const style = {
+  top: 120,
+  left: 500,
+  lineHeight: '24px'
+};
 
 export function TwoLevelPieChart() {
   return (
     <RadarChart cx={300} cy={250} outerRadius={150} width={600} height={500} data={data}>
       <PolarGrid />
       <PolarAngleAxis dataKey="subject" />
-      <PolarRadiusAxis angle={30} domain={[0, 150]} />
-      <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-      <Radar name="Lily" dataKey="B" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6} />
-      <Legend />
+      <PolarRadiusAxis angle={0} domain={[0, 100]} />
+      <Radar name="Dostępność towaru w magazynach [%]" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+      <Legend iconSize={10} width={220} height={140} layout="vertical" verticalAlign="middle" wrapperStyle={style} />
     </RadarChart>
   );
 }
