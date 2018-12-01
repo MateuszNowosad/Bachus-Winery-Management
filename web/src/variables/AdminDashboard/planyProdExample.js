165082792034147146664af2f100128f135f7395
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-12-01 15:23:34 +0100

    Test data for ProductionPlanDetails.

---
+++ web/src/variables/AdminDashboard/planyProdExample.js
@@ -0,0 +1,15 @@
+const planyProdExample = {
+  data: {
+    planProd: [
+      {
+        idPlanu: '0',
+        nazwa: 'Wino dobre',
+        Opis:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        Edokument: 'test.pdf'
+      }
+    ]
+  }
+};
+
+export default planyProdExample;
