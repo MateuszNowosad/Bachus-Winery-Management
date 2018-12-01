3652fca1b279b8ab55c4349589599d614f3e37ea
Jakub Mielniczuk <mielniczuk.jakub@gmail.com>
2018-12-01 18:52:25 +0100

    New query to get all data from DictOperacjeNaWinnicy

---
+++ web/src/queries/getDictVineyardOperations.js
@@ -0,0 +1,12 @@
+import gql from "graphql-tag"
+
+const getDictVineyardOperations = gql`
+{
+  DictOperacjeNaWinnicy {
+    idDictOperacjeNaWinnicy
+    nazwa
+    opis
+  }
+}`;
+
+export default getDictVineyardOperations;
