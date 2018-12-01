b60220dbfd6cf4831818b06e51ceee35fb19d4c2
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-12-01 21:41:55 +0100

    Labels for few tables.

---
+++ web/src/localisation/DatabaseLabels.js
@@ -0,0 +1,28 @@
+export const userDatabaseLabels = {
+  idUzytkownika: 'ID Użytkownika',
+  imie: 'Imie',
+  nazwisko: 'Nazwisko',
+  login: 'Login',
+  haslo: 'Hasło',
+  PESEL: 'PESEL',
+  eMail: 'E-Mail',
+  nrTelefonu: 'Nr telefonu',
+  rola: 'Rola użytkownika',
+  dataOstatniegoLogowania: 'Data ostatniego logowania'
+};
+
+export const addressDatabaseLabels = {
+  idAdres: 'ID Adresu',
+  miasto: 'Miasto',
+  kodPocztowy: 'Kod pocztowy',
+  ulica: 'Ulica',
+  nrLokalu: 'Nr lokalu',
+  nrPosesji: 'Nr posesji'
+};
+
+export const categoriesDatabaseLabels = {
+  idKategorie: 'ID Kategorii',
+  nazwa: 'Nazwa',
+  jednostka: 'Jednostka',
+  opis: 'Opis'
+};
