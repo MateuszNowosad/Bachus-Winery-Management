7a8e9f73a82edb1bfb6bc5b59e72c54a9d95164b
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-12-03 03:36:59 +0100

    Added port forwarding for docker container.

--- server/scripts/dockerSetup.sh
+++ server/scripts/dockerSetup.sh
@@ -4,4 +4,4 @@ echo "Prepare mysql image"
 docker pull mysql/mysql-server:5.7
 
 echo "Start mysql container"
-docker run --name=bachus-winery57 -d mysql/mysql-server:5.7
+docker run --name=bachus-winery57 -d -p 3306:3306 mysql/mysql-server:5.7
