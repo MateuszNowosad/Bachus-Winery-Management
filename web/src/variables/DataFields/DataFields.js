25b16a7a3ed2aaf3c04a3e3093c9a9e3e8521a4f
Jakub Mielniczuk <mielniczuk.jakub@gmail.com>
2018-12-08 16:36:25 +0100

    Added variables with possible data fields in database

---
+++ web/src/variables/DataFields/DataFields.js
@@ -0,0 +1,13 @@
+const DataFields = [
+  'dataPoczatku',
+  'dataZakonczenia',
+  'data',
+  'dataUtworzenia',
+  'dataPrzyjecia',
+  'dataWydania',
+  'dataOstatniegoLogowania',
+  'dataOstatniegoZbioru',
+  'dataWinobrania'
+];
+
+export default DataFields;
