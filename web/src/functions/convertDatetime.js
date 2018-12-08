eab8592e7da7b2b6a0ed076e2d7b7e10ee11fc93
Jakub Mielniczuk <mielniczuk.jakub@gmail.com>
2018-12-08 19:30:20 +0100

    Fix to convertDatetime function

--- web/src/functions/convertDatetime.js
+++ web/src/functions/convertDatetime.js
@@ -1,7 +1,7 @@
-const convertDate = date => {
-  const convertedDate = new Date(date);
-  let dateString = convertedDate.toISOString();
-  return dateString.replace('T', ' ').slice(0, -5);
+const convertDatetime = date => {
+  let newDate = new Date(Number(date));
+  let converted = newDate.toISOString();
+  return converted.replace('T', ' ').slice(0, -5);
 };
 
-export default convertDate;
+export default convertDatetime;
