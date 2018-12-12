59d26ee3e09a76d116b7933bfb4b38c721f0be6e
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-12-12 02:51:03 +0100

    Warehouse details connected to database.

--- web/src/views/AdminDashboard/Subpages/WarehouseDetails.js
+++ web/src/views/AdminDashboard/Subpages/WarehouseDetails.js
@@ -2,12 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AdminDashboardStyle from '../../../assets/jss/common/views/AdminDashboard/AdminDashboardStyle.js';
+import AutoTable from '../../../components/AutoTable/AutoTable';
+import { Query } from 'react-apollo';
+import getItemsFromWarehouse from '../../../queries/getItemsFromWarehouse';
 
 class WarehouseDetails extends React.Component {
   render() {
+    let warehouseId = this.props.match.params.id;
     return (
       <React.Fragment>
-        <p>test {this.props.match.params.id}</p>
+        <Query query={getItemsFromWarehouse(warehouseId)}>
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error :(</p>;
+            console.log(data.Magazyn);
+            return (
+              <AutoTable
+                queryData={data.Magazyn[0].pozycjaWMagazynie}
+                querySize={data.Magazyn[0].pozycjaWMagazynie.length}
+                editMode={false}
+              />
+            );
+          }}
+        </Query>
       </React.Fragment>
     );
   }
