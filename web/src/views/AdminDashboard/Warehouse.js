38103693d54b20511e5ddb6ca7d65295acae3c6b
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-12-12 01:00:50 +0100

    Added right drawer template.

--- web/src/views/AdminDashboard/Warehouse.js
+++ web/src/views/AdminDashboard/Warehouse.js
@@ -3,14 +3,34 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import AdminDashboardStyle from '../../assets/jss/common/views/AdminDashboard/AdminDashboardStyle.js';
+import List from '@material-ui/core/List/List';
+import Divider from '@material-ui/core/Divider/Divider';
+import Drawer from '@material-ui/core/Drawer/Drawer';
+import ListItem from '@material-ui/core/ListItem/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText/ListItemText';
+import InboxIcon from '@material-ui/icons/Inbox';
+import MailIcon from '@material-ui/icons/Mail';
 
 class Warehouse extends React.Component {
   render() {
+    const { classes } = this.props;
     return (
       <React.Fragment>
         <Typography variant="h4" gutterBottom component="h2">
           Magazyny
         </Typography>
+        <Drawer className={classes.drawer} variant="permanent" anchor="right">
+          <div className={classes.appBarSpacer} />
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
       </React.Fragment>
     );
   }
