467c7f888c87ef93f648b06bd4359507f1535c43
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-12-18 22:09:56 +0100

    UserInfo now fetches logged in user correctly.

--- server/src/app.js
+++ server/src/app.js
@@ -3,9 +3,8 @@ import session from 'express-session';
 import cors from 'cors';
 import * as bodyParser from 'body-parser';
 import path from 'path';
-import * as sequelize from "./sequelizeDB";
-import bcrypt from "bcrypt";
-
+import * as sequelize from './sequelizeDB';
+import bcrypt from 'bcrypt';
 
 //TODO Don't forget to change this file when refractoring routes.
 const SESSION_SECRET = 'SECRET'; //TODO place this in .env before production
@@ -23,61 +22,59 @@ export const createApp = async () => {
   // };
 
   const corsOptions = {
-    origin: "http://localhost:3000",
-    credentials: true,
+    origin: 'http://localhost:3000',
+    credentials: true
   };
 
   app.use(cors(corsOptions));
   app.use(urlencoded({ extended: true }));
   app.use(bodyParser.json());
 
-
-
   app.use(
     session({
-      name: "loginID",
+      name: 'loginID',
       secret: SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
       cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: process.env.NODE_ENV === 'production',
         maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
       }
     })
   );
 
-  app.use( (req, res, next) => {
+  app.use((req, res, next) => {
     console.log('req.session', req.session);
-    next()
+    next();
   });
 
-  const salt = await bcrypt
+  const salt = await bcrypt;
 
   app.post('/usrauthorization', async (req, res) => {
     console.log('user signup');
     console.log(req.body);
-    const user = await sequelize.getUzytkownicy({login: req.body.login});
+    const user = await sequelize.getUzytkownicy({ login: req.body.login });
     console.log('60, user Mateusz: ', user);
-    if(!user.length){
+    if (!user.length) {
       res.end();
     }
-     if(user[0].czyAktywne!==1) {
-       res.end();
-     }
-
+    if (user[0].czyAktywne !== 1) {
+      res.end();
+    }
 
     req.session.login = req.body.login;
+    req.session.idUzytkownika = user[0].idUzytkownika;
     req.session.role = user[0].dictRolaUzytkownikowIdRolaUzytkownikow;
-    res.send({status: "Success", cookie: req.session.cookie});
+    res.send({ status: 'Success', cookie: req.session.cookie });
     res.end();
   });
 
   app.get('/usrrole', (req, res) => {
     console.log('user rolelookup');
     console.log(req.body);
-    res.send({ role: req.session.role });
-    res.end()
+    res.send({ idUzytkownika: req.session.idUzytkownika, role: req.session.role });
+    res.end();
   });
 
   app.post('/usrlogout', async (req, res) => {
@@ -87,63 +84,62 @@ export const createApp = async () => {
     res.end();
   });
 
-
-//   app.use(session({ key: 'testCookie', secret: 'cat', cookie: { maxAge: 3*100*100 } }));
-//
-//   const sessionChecker = (req, res, next) => {
-//     if (req.session.user && req.cookies.user_sid) {
-//       res.redirect('/admindashboard');
-//     } else {
-//       next();
-//     }
-//   };
-//
-//   app.get('/', sessionChecker, (req, res) => {
-//     res.redirect('/');
-//   });
-//
-//   // app.get('/session', function(req, res, next) {
-//   //   if (req.session.views) {
-//   //     req.session.views++;
-//   //     res.setHeader('Content-Type', 'text/html');
-//   //     res.write('<p>views: ' + req.session.views + '</p>');
-//   //     res.write('<p>expires in: ' + req.session.cookie.maxAge / 1000 + 's</p>');
-//   //     res.end();
-//   //   } else {
-//   //     req.session.views = 1;
-//   //     res.end('welcome to the session demo. refresh!');
-//   //   }
-//   // });
-//
-//   app.route('/')
-//     .get(sessionChecker, (req, res) => {
-//       res.sendFile(__dirname + '/public/login.html');
-//     })
-//     .post((req, res) => {
-//       var username = req.body.username,
-//         password = req.body.password;
-//
-//       User.findOne({ where: { username: username } }).then(function (user) {
-//         if (!user) {
-//           res.redirect('/login');
-//         } else if (!user.validPassword(password)) {
-//           res.redirect('/login');
-//         } else {
-//           req.session.user = user.dataValues;
-//           res.redirect('/dashboard');
-//         }
-//       });
-//     });
-//
-//   app.get('/admindashboard', (req, res) => {
-//     if (req.session.user && req.cookies.user_sid) {
-//       res.sendFile(__dirname + '/public/dashboard.html');
-//     } else {
-//       res.redirect('/login');
-//     }
-//   });
-//
-   return { app };
+  //   app.use(session({ key: 'testCookie', secret: 'cat', cookie: { maxAge: 3*100*100 } }));
+  //
+  //   const sessionChecker = (req, res, next) => {
+  //     if (req.session.user && req.cookies.user_sid) {
+  //       res.redirect('/admindashboard');
+  //     } else {
+  //       next();
+  //     }
+  //   };
+  //
+  //   app.get('/', sessionChecker, (req, res) => {
+  //     res.redirect('/');
+  //   });
+  //
+  //   // app.get('/session', function(req, res, next) {
+  //   //   if (req.session.views) {
+  //   //     req.session.views++;
+  //   //     res.setHeader('Content-Type', 'text/html');
+  //   //     res.write('<p>views: ' + req.session.views + '</p>');
+  //   //     res.write('<p>expires in: ' + req.session.cookie.maxAge / 1000 + 's</p>');
+  //   //     res.end();
+  //   //   } else {
+  //   //     req.session.views = 1;
+  //   //     res.end('welcome to the session demo. refresh!');
+  //   //   }
+  //   // });
+  //
+  //   app.route('/')
+  //     .get(sessionChecker, (req, res) => {
+  //       res.sendFile(__dirname + '/public/login.html');
+  //     })
+  //     .post((req, res) => {
+  //       var username = req.body.username,
+  //         password = req.body.password;
+  //
+  //       User.findOne({ where: { username: username } }).then(function (user) {
+  //         if (!user) {
+  //           res.redirect('/login');
+  //         } else if (!user.validPassword(password)) {
+  //           res.redirect('/login');
+  //         } else {
+  //           req.session.user = user.dataValues;
+  //           res.redirect('/dashboard');
+  //         }
+  //       });
+  //     });
+  //
+  //   app.get('/admindashboard', (req, res) => {
+  //     if (req.session.user && req.cookies.user_sid) {
+  //       res.sendFile(__dirname + '/public/dashboard.html');
+  //     } else {
+  //       res.redirect('/login');
+  //     }
+  //   });
+  //
+  return { app };
 };
 
 module.exports = {
