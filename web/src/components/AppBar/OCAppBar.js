84d60529ef8ba61da32a39f025d00b96dda1c096
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2018-12-18 23:28:24 +0100

    Unused buttons removed from AppBar.

--- web/src/components/AppBar/OCAppBar.js
+++ web/src/components/AppBar/OCAppBar.js
@@ -14,6 +14,7 @@ import MailIcon from '@material-ui/icons/Mail';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import OCAppBarStyle from '../../assets/jss/common/components/OCAppBarStyle.js';
+import Link from "react-router-dom/es/Link";
 
 class OCAppBar extends React.Component {
   handleClick = () => {
@@ -22,6 +23,7 @@ class OCAppBar extends React.Component {
   };
 
   render() {
+    const profileLink = props => <Link to="/admindashboard/profile" {...props} />
     const { classes, drawerOpen } = this.props;
     return (
       <React.Fragment>
@@ -38,17 +40,7 @@ class OCAppBar extends React.Component {
             <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
               Panel sterowania
             </Typography>
-            <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton color="inherit">
-              <Badge badgeContent={17} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton color="inherit">
+            <IconButton color="inherit" component={profileLink}>
               <AccountCircle />
             </IconButton>
           </Toolbar>
